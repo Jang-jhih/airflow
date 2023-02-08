b9524dd8bf7b3ddfc4d9765b2c723b0425bf45ef
Jacob <87475035+Jang-jhih@users.noreply.github.com>
2023-02-08 11:21:42 +0800

    test

--- start.sh
+++ start.sh
@@ -5,12 +5,14 @@
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 
+
+
+
+# docker compose -f ./docker-amundsen.yml  up -d
+
+docker compose -f ./test.yml  up -d
 docker compose -f ./docker-airflow.yml up -d
 
-
-docker compose -f ./docker-amundsen.yml  up -d
-
-
-# docker compose -f ./airflow/docker-airflow.yml exec -it airflow-webserver bash
-# docker compose -f ./airflow/docker-airflow.yml exec -it postgres bash
+# docker compose -f ./docker-airflow.yml exec -it airflow-webserver bash
+# docker compose -f ./docker-airflow.yml exec -it postgres bash
 # psql -d airflow -U airflow
