1506fd4af390c6fa33ca8a8d75b7b83978a13bd0
Jacob <87475035+Jang-jhih@users.noreply.github.com>
2023-02-15 23:48:00 +0800

    test

--- start.sh
+++ start.sh
@@ -16,3 +16,6 @@ docker compose -f ./docker-airflow.yml up -d
 # docker compose -f ./docker-airflow.yml exec -it airflow-webserver bash
 # docker compose -f ./docker-airflow.yml exec -it postgres bash
 # psql -d airflow -U airflow
+
+# docker compose -f ./airflow/docker-airflow.yml run -it --rm  postgres  bash
+docker compose  -f ./airflow/docker-airflow.yml up  postgres  -d
